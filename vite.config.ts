32b552a9c0daf9622ab9c35500be3434eefff4ca
John Amaka <39513553+John-AE@users.noreply.github.com>
2025-09-22 06:34:21 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -25,7 +25,7 @@ export default defineConfig({
   },
   root: path.resolve(import.meta.dirname, "client"),
   build: {
-  outDir: path.resolve(import.meta.dirname, "dist"), // Remove /public
+  outDir: path.resolve(import.meta.dirname, "dist/public"), // Remove /public
   emptyOutDir: true, },
   server: {
     fs: {

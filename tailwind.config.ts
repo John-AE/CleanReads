bd3f60a1acbb964cd99cb9874762cc5070ab41da
John Amaka <39513553+John-AE@users.noreply.github.com>
2025-09-22 06:15:04 -0400

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,11 +2,7 @@ import type { Config } from "tailwindcss";
 
 export default {
   darkMode: ["class"],
-  content: [
-  "./index.html",
-  "./src/**/*.{js,ts,jsx,tsx}",
-  "./client/**/*.{js,ts,jsx,tsx}"
-],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}","./client/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       borderRadius: {
